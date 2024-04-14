9c00a80c113b5ca7f7c502b1fb3a54447a1b4084
ShinMinOh <luciano97@naver.com>
2024-04-14 15:41:40 +0900

    [Feat] 테스트용 api & Entity 생성

--- src/main/java/com/project/review/api/TestApi.java
+++ src/main/java/com/project/review/api/TestApi.java
@@ -1,6 +1,9 @@
 package com.project.review.api;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +11,19 @@ public class TestApi {
 
   @GetMapping("/hello/world")
   public String helloWorld(){
-    return "Hello, world!";
+    return "[GET] Hello, world!";
+  }
+
+  @PostMapping("/hello/world")
+  public String posthelloWorld(){
+    return "[Post] Hello, world!";
+  }
+  @PutMapping("/hello/world")
+  public String puthelloWorld(){
+    return "[Put] Hello, world!";
+  }
+  @DeleteMapping("/hello/world")
+  public String deletehelloWorld(){
+    return "[Delete] Hello, world!";
   }
 }
