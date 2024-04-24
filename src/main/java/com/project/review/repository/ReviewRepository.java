af0deba4e22b34d3c8ded556c5e35f675858b6d9
ShinMinOh <luciano97@naver.com>
2024-04-24 18:39:39 +0900

    [Feat] Querydsl 구현

--- src/main/java/com/project/review/repository/ReviewRepository.java
+++ src/main/java/com/project/review/repository/ReviewRepository.java
@@ -3,6 +3,6 @@ package com.project.review.repository;
 import com.project.review.model.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
 
 }
